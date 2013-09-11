4b474f4d75d0c1f3c08155d65f442151b87d6c3d
vincentt143 <tran.vincent17@gmail.com>
2013-09-11 22:21:06 +1000

    Add script to get parties from AEC

---
+++ app/scrapers/aec_party_scraper.rb
@@ -0,0 +1,19 @@
+# Grabs a list of all political parties that ran for the election
+# from the Australian Electoral Commission website
+
+require 'nokogiri'
+require 'open-uri'
+
+Party.delete_all
+
+doc = Nokogiri::HTML(open("http://www.aec.gov.au/parties_and_representatives/party_registration/Registered_parties/"))
+
+list = []
+
+doc.search("strong.r-party a").map {|a| list << a}
+
+list.each {|p|
+    party = Party.new
+    party.name = p.inner_html
+    party.save
+  }
